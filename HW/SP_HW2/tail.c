ed24b58240df4c56553c2ff6573fabb971ae4e00
jim2832 <jim880705@gmail.com>
2023-09-20 16:59:45 +0800

    230920 update

---
+++ HW/SP_HW2/tail.c
@@ -0,0 +1,6 @@
+/*
+The command tail [ –n num ] file prints the last num lines (ten by default) of the
+named file. Implement this command using I/O system calls (lseek(), read(), write(),
+and so on). Keep in mind the buffering issues described in this chapter, in order to
+make the implementation efficient.
+*/
