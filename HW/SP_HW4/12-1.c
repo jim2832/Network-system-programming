73ad35866f0ba6caf16e5f38e432f5f7d564c08b
jim2832 <jim880705@gmail.com>
2023-10-04 23:31:33 +0800

    231004 update

--- HW/SP_HW4/12-1.c
+++ HW/SP_HW4/12-1.c
@@ -9,6 +9,31 @@
 #include <pwd.h>
 #include <errno.h>
 
+void removeFirstNCharacters(char *str, int n){
+    int length = strlen(str);
+    if(n >= length){
+        // If n is greater than or equal to the length of the string,
+        // remove the entire string(set it to an empty string).
+        str[0] = '\0';
+    } else{
+        // Move characters to the left to remove the first n characters
+        for(int i = 0; i <= length - n; i++){
+            str[i] = str[i + n];
+        }
+        str[length - n] = '\0';  // Null-terminate the new string
+    }
+}
+
+void removeTrailingNewline(char *str) {
+    int length = strlen(str);
+
+    // Check if the last character is a newline
+    if(length > 0 && str[length - 1] == '\n'){
+        // Replace the newline with a null terminator
+        str[length - 1] = '\0';
+    }
+}
+
 /* Return UID corresponding to 'name', or -1 on error */
 uid_t userIdFromName(const char *name){
     struct passwd *pwd;
@@ -37,13 +62,6 @@ uid_t userIdFromName(const char *name){
 }
 
 void listProcessesForUser(const char *username){
-    /* get the user ID from name */
-    uid_t uid = userIdFromName(username);
-    printf("%d", uid);
-    if(uid == -1){
-        fprintf(stderr, "Error: Unable to find user %s\n", username);
-        return;
-    }
 
     /* open the directory */
     DIR *dir = opendir("/proc");
@@ -54,52 +72,54 @@ void listProcessesForUser(const char *username){
 
     struct dirent *entity;
     char path[1024];
+    int count = 0;
 
     // DT_DIR(directory) -> 4
     // DT_REG(file) -> 8
     while((entity = readdir(dir)) != NULL){
-        // Check if the entity is a directory and represents a process ID
+
+        /* Check if the entity is a directory and represents a process ID */
         if(entity->d_type == 4 && atoi(entity->d_name) != 0){ // It's a directory
             sprintf(path, "/proc/%s/status", entity->d_name);
-            // printf("%s\n", path);
 
-            // Open the status file for the process
-            int fd = open(path, O_RDONLY);
-            if(fd == -1){
-                // Process directory may have disappeared, skip it
-                continue;
-            }
+            /* parse the status file */
+            FILE *file = fopen(path, "r");
+            char *line = NULL;
+            size_t len = 0;
+            int line_number = 0;
+            char pid[256] = {0};
+            char process[256] = {0};
 
-            // Read the contents of the status file
-            char buffer[4096];
-            ssize_t bytesRead = read(fd, buffer, sizeof(buffer) - 1);
-            if(bytesRead > 0){
-                buffer[bytesRead] = '\0';
-                char *token = strtok(buffer, "\n");
+            while(getline(&line, &len, file) != EOF && line_number++ < 10){
 
-                // Parse and extract process ID and command name
-                char *pid = NULL;
-                char *command = NULL;
-                while(token != NULL){
-                    if(strncmp(token, "Pid:", 4) == 0){
-                        pid = strtok(token + 4, " \t");
+                // process name
+                if(line_number == 1){
+                    strcpy(process, line);
+                    removeFirstNCharacters(process, 6);
+                    removeTrailingNewline(process);
+                }
+                // pid
+                else if(line_number == 6){
+                    strcpy(pid, line);
+                    removeFirstNCharacters(pid, 5);
+                    removeTrailingNewline(pid);
+                }
+                // uid
+                else if(line_number == 9){
+                    char delim[] = " \t\n"; /* SPACE or TAB or NL */
+                    strtok(line, delim);
+                    strtok(NULL, delim);
+                    uid_t uid = atoi(strtok(NULL, delim));
+
+                    // Only list the processes that are run by <username>
+                    if(uid == userIdFromName(username)){
+                        count++;
+                        printf("%d. Proess ID: %s, Process: %s\n", count, pid, process);
                     }
-                    else if(strncmp(token, "Name:", 5) == 0){
-                        command = strtok(token + 5, " \t");
-                    }
-
-                    if(pid != NULL && command != NULL)
-                        break;
-
-                    token = strtok(NULL, "\n");
                 }
 
-                if(pid != NULL && command != NULL && atoi(pid) != 0){
-                    printf("Process ID: %s, Command: %s\n", pid, command);
-                }
             }
-
-            close(fd);
+            fclose(file);
         }
     }
 
@@ -112,7 +132,9 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
-    listProcessesForUser(argv[1]);
+    char *user = argv[1];
+    printf("\nThe processes that are run by user '%s' are as followed:\n\n", user);
+    listProcessesForUser(user);
 
     return 0;
 }
