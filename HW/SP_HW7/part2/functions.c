2c1a16f7d2385bd16663fcea41ee01922c2d0da3
jim2832 <jim880705@gmail.com>
2023-11-02 19:23:52 +0800

    231102 update hw7

--- HW/SP_HW7/part2/functions.c
+++ HW/SP_HW7/part2/functions.c
@@ -1,49 +1,67 @@
-#define _POSIX_SOURCE 1
+#define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <signal.h>
+#include <unistd.h>
 
-typedef void (*sighandler_t)(int);
-
-sighandler_t sigset(int signum, sighandler_t handler) {
-    struct sigaction new_action, old_action;
-    
-    new_action.sa_handler = handler;
-    sigemptyset(&new_action.sa_mask);
-    new_action.sa_flags = 0;
-    
-    if (signum == SIGALRM || signum == SIGCHLD) {
-        new_action.sa_flags |= SA_RESTART;
-    }
-    
-    if (sigaction(signum, &new_action, &old_action) < 0) {
-        return SIG_ERR;
-    }
-    
-    return old_action.sa_handler;
+// Set the signal handler for the given signal
+void _sigset(int signum, void(*handler)(int)){
+    struct sigaction sa;
+    sa.sa_handler = handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(signum, &sa, NULL);
 }
 
-int sighold(int signum) {
+// Block the given signal
+void _sighold(int signum){
+    sigset_t blocked;
+    sigemptyset(&blocked);
+    sigaddset(&blocked, signum);
+    sigprocmask(SIG_BLOCK, &blocked, NULL);
+}
+
+// Unblock the given signal
+void _sigrelse(int signum){
+    sigset_t unblocked;
+    sigemptyset(&unblocked);
+    sigaddset(&unblocked, signum);
+    sigprocmask(SIG_UNBLOCK, &unblocked, NULL);
+}
+
+// Ignore the given signal
+void _sigignore(int signum){
+    _sigset(signum, SIG_IGN);
+}
+
+// Suspend the process until a signal is received
+void _sigpause(){
     sigset_t mask;
     sigemptyset(&mask);
-    sigaddset(&mask, signum);
-    return sigprocmask(SIG_BLOCK, &mask, NULL);
-}
-
-
-int sigrelse(int signum) {
-    sigset_t mask;
-    sigemptyset(&mask);
-    sigaddset(&mask, signum);
-    return sigprocmask(SIG_UNBLOCK, &mask, NULL);
-}
-
-sighandler_t sigignore(int signum) {
-    return sigset(signum, SIG_IGN);
-}
-
-void sigpause(int signum) {
-    sigset_t mask;
-    sigemptyset(&mask);
-    sigaddset(&mask, signum);
     sigsuspend(&mask);
+}
+
+// Send the given signal to the given process
+void _signal_handler(int signum){
+    printf("Received signal %d\n", signum);
+}
+
+int main(int argc, char *argv[]){
+    _sigset(SIGUSR1, _signal_handler); // Set the signal handler for SIGUSR1
+    _sighold(SIGUSR1); // Block SIGUSR1
+    _sigrelse(SIGUSR1); // Unblock SIGUSR1
+
+    pid_t pid = fork(); // Fork a child process
+
+    if(pid == 0){ // Child process
+        _sigpause(); // Suspend the process until a signal is received
+        printf("Child process received signal\n");
+    }
+    else{ // Parent process
+        printf("Parent process sleep for 3 second then send signal to child\n");
+        sleep(3); // Sleep for 3 seconds
+        kill(pid, SIGUSR1); // Send SIGINT to the child process
+        printf("Parent process sent signal\n");
+    }
+
+    return 0;
 }
