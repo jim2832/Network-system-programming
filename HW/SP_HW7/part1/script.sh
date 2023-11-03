92be49238e3fcd7e7f327e36dfe85f90a705cf09
jim2832 <jim880705@gmail.com>
2023-11-03 23:36:35 +0800

    231103 update hw7

--- HW/SP_HW7/part1/script.sh
+++ HW/SP_HW7/part1/script.sh
@@ -5,23 +5,25 @@ test_count=10
 # original version
 make all
 for i in $(seq 1 $test_count); do
+    echo Processing origin version $i/$test_count
+
     # run command and save output to variable
     output=$({ time ./origin 100000; } 2>&1)
 
     # use awk to extract the seconds of real and append to file
     echo -n "$output" | grep real | awk '{print $2}' | sed 's/^0m//' >> origin_output.txt
-    echo Processing origin version $i/$test_count
 done
 
 # new version
 make new
 for i in $(seq 1 $test_count); do
+    echo Processing new version $i/$test_count
+    
     # run command and save output to variable
     output=$({ time ./new_version 100000; } 2>&1)
 
     # use awk to extract the seconds of real and append to file
     echo -n "$output" | grep real | awk '{print $2}' | sed 's/^0m//' >> new_output.txt
-    echo Processing new version $i/$test_count
 done
 
 # merge files
